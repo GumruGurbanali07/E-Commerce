6991984a423fa5ecc798049412ab3111d65d355f
GumruGurbanali07 <qurbaneliyevaqumru@gmail.com>
2024-09-30 10:35:42 +0400

    commit

--- ECommerceCLI/src/app/app.module.ts
+++ ECommerceCLI/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { BaseComponent } from './base/base.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,10 +22,11 @@ import { BaseComponent } from './base/base.component';
     AdminModule,
     UiModule,
     ToastrModule.forRoot(),
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    HttpClientModule
   ],
   providers: [
-    provideClientHydration()
+   {provide:"baseUrl", useValue:"https://localhost:7191/api", multi:true}
   ],
   bootstrap: [AppComponent]
 })
