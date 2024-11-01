6ee7aab6da2ef89ab17a40468f736da809f52603
GumruGurbanali07 <qurbaneliyevaqumru@gmail.com>
2024-11-01 22:32:03 +0400

    commit

--- ECommerceAPI/Infrastructure/ECommerceAPI.Persistence/ServiceRegistration.cs
+++ ECommerceAPI/Infrastructure/ECommerceAPI.Persistence/ServiceRegistration.cs
@@ -29,6 +29,7 @@ namespace ECommerceAPI.Persistence
 			services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
 			services.AddScoped<IUserService, UserService>();
 			services.AddScoped<IAuthService, AuthService>();
+			services.AddScoped<IBasketService, BasketService>();
 			services.AddScoped<IBasketReadRepository, BasketReadRepository>();
 			services.AddScoped<IBasketWriteRepository, BasketWriteRepository>();
 			services.AddScoped<IBasketItemReadRepository, BasketItemReadRepository>();
--- ECommerceAPI/Presentation/ECommerceAPI.API/Controllers/BasketController.cs
+++ ECommerceAPI/Presentation/ECommerceAPI.API/Controllers/BasketController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ECommerceAPI.Application.Features.Commands.Basket.AddItemToBasket;
+using ECommerceAPI.Application.Features.Commands.Basket.RemoveBasketItem;
+using ECommerceAPI.Application.Features.Commands.Basket.UpdateQuantity;
+using ECommerceAPI.Application.Features.Queries.Basket;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceAPI.API.Controllers
@@ -7,5 +12,43 @@ namespace ECommerceAPI.API.Controllers
 	[ApiController]
 	public class BasketController : ControllerBase
 	{
+		readonly IMediator _mediator;
+
+		public BasketController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		[HttpGet]
+		//[AuthorizeDefinition(Menu = AuthorizeDefinitionConstants.Baskets, ActionType = ActionType.Reading, Definition = "Get Basket Items")]
+		public async Task<IActionResult> GetBasketItems([FromQuery] GetBasketItemQueryRequest getBasketItemsQueryRequest)
+		{
+			List<GetBasketItemQueryResponse> response = await _mediator.Send(getBasketItemsQueryRequest);
+			return Ok(response);
+		}
+
+		[HttpPost]
+		//[AuthorizeDefinition(Menu = AuthorizeDefinitionConstants.Baskets, ActionType = ActionType.Writing, Definition = "Add Item To Basket")]
+		public async Task<IActionResult> AddItemToBasket(AddItemToBasketCommandRequest addItemToBasketCommandRequest)
+		{
+			AddItemToBasketCommandResponse response = await _mediator.Send(addItemToBasketCommandRequest);
+			return Ok(response);
+		}
+
+		[HttpPut]
+		//[AuthorizeDefinition(Menu = AuthorizeDefinitionConstants.Baskets, ActionType = ActionType.Updating, Definition = "Update Quantity")]
+		public async Task<IActionResult> UpdateQuantity(UpdateQuantityCommandRequest updateQuantityCommandRequest)
+		{
+			UpdateQuantityCommandResponse response = await _mediator.Send(updateQuantityCommandRequest);
+			return Ok(response);
+		}
+
+		[HttpDelete("{BasketItemId}")]
+		//[AuthorizeDefinition(Menu = AuthorizeDefinitionConstants.Baskets, ActionType = ActionType.Deleting, Definition = "Remove Basket Item")]
+		public async Task<IActionResult> RemoveBasketItem([FromRoute] RemoveBasketItemCommandRequest removeBasketItemCommandRequest)
+		{
+			RemoveBasketItemCommandResponse response = await _mediator.Send(removeBasketItemCommandRequest);
+			return Ok(response);
+		}
 	}
 }
